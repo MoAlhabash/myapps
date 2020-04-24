121177ad089fc965e3d6699bb45036dfcfbd089c
Mouhamad Alhabash <me@mouhamadalhabash.com>
2020-04-24 22:03:46 +0200

    Initial commit

---
+++ Car.java
@@ -0,0 +1,29 @@
+public class Car
+{
+    private String color;
+    private String manufacturer;
+    private int doors;
+
+    public Car(String color, String manufacturer, int doors)
+    {
+        this.color = color;
+        this.manufacturer = manufacturer;
+        this.doors = doors;
+    }
+    // mouhamad Alhabash
+
+    public String getColor()
+    {
+        return color;
+    }
+
+    public String getManufacturer()
+    {
+        return manufacturer;
+    }
+
+    public int getDoors()
+    {
+        return doors;
+    }
+}
